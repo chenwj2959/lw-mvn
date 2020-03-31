b29ab5f89db8fce8456b806fa1f6d5d31ce3ff25
wenjie chen <285325171@qq.com>
2020-03-31 22:21:07 +0800

    解析HTTP REQUEST报文(Writing)

--- src/main/java/com/cwj/mvn/framework/http/bean/HttpRequest.java
+++ src/main/java/com/cwj/mvn/framework/http/bean/HttpRequest.java
@@ -1,16 +1,43 @@
 package com.cwj.mvn.framework.http.bean;
 
 public class HttpRequest {
+    
+    private static final String LINE_BREAK = "\\n\\r";
+    private static final String SPLIT_BREAK = " ";
+    private static final String QUESTION_MASK = "?";
+    private static final String PARAMETER_BREAK = "&";
+    private static final String PARAMETER_VALUE_BREAK = "=";
 
-    private String protocol; // http or https
+    private String protocol; // HTTP/1.0 or HTTP/1.1
     private String method; // support GET and POST
-    private String domain;
     private String route;
     private HttpHeader headers;
     private HttpParameter parameters;
     
-    public HttpRequest(byte[] buffer) {
+    public HttpRequest(String requestStr) {
+        String[] requestArr = requestStr.split(LINE_BREAK);
+        if (requestArr.length == 0) {
+            throw new RuntimeException("Http request string format error");
+        }
+        String[] urlArr = requestArr[0].split(SPLIT_BREAK);
+        if (urlArr.length != 3) {
+            throw new RuntimeException("URL format error");
+        }
+        headers = new HttpHeader();
+        parameters = new HttpParameter();
         
+        method = urlArr[0];
+        route = urlArr[1]; // TODO 路由要减去默认部分, 要解析GET参数
+        protocol = urlArr[2];
+        
+        int questionMaskIndex = route.indexOf(QUESTION_MASK); // 解析跟在URL后面的参数
+        if (questionMaskIndex != -1 && questionMaskIndex != route.length() - 1) {
+            route = route.substring(0, questionMaskIndex);
+            String[] parametersArr = route.substring(questionMaskIndex + 1).split(PARAMETER_BREAK);
+            for (String parametersStr : parametersArr) {
+                
+            }
+        }
     }
     
     public String getProtocol() {
@@ -24,12 +51,6 @@ public class HttpRequest {
     }
     public void setMethod(String method) {
         this.method = method;
-    }
-    public String getDomain() {
-        return domain;
-    }
-    public void setDomain(String domain) {
-        this.domain = domain;
     }
     public String getRoute() {
         return route;
--- src/main/java/com/cwj/mvn/utils/StringUtils.java
+++ src/main/java/com/cwj/mvn/utils/StringUtils.java
@@ -2,4 +2,7 @@ package com.cwj.mvn.utils;
 
 public class StringUtils {
 
+    public static boolean isEmpty(CharSequence charSeq) {
+        return charSeq == null || charSeq.length() == 0;
+    }
 }
