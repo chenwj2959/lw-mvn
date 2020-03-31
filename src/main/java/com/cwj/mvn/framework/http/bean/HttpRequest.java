e7cf955be0ac1b0a5b36a26386311a579332e820
chenwj2959 <285325171@qq.com>
2020-03-31 17:47:12 +0800

    测试Nexus返回POM和Jar时的HTTPHeader

--- src/main/java/com/cwj/mvn/framework/http/bean/HttpRequest.java
+++ src/main/java/com/cwj/mvn/framework/http/bean/HttpRequest.java
@@ -6,4 +6,47 @@ public class HttpRequest {
     private String method; // support GET and POST
     private String domain;
     private String route;
+    private HttpHeader headers;
+    private HttpParameter parameters;
+    
+    public HttpRequest(byte[] buffer) {
+        
+    }
+    
+    public String getProtocol() {
+        return protocol;
+    }
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+    public String getMethod() {
+        return method;
+    }
+    public void setMethod(String method) {
+        this.method = method;
+    }
+    public String getDomain() {
+        return domain;
+    }
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+    public String getRoute() {
+        return route;
+    }
+    public void setRoute(String route) {
+        this.route = route;
+    }
+    public HttpHeader getHeaders() {
+        return headers;
+    }
+    public void setHeaders(HttpHeader headers) {
+        this.headers = headers;
+    }
+    public HttpParameter getParameters() {
+        return parameters;
+    }
+    public void setParameters(HttpParameter parameters) {
+        this.parameters = parameters;
+    }
 }
